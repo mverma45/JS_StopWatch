da75339826b68bc91d59bfcdf13bb0c5080ea5d9
Manny Verma <mannyverma11@gmail.com>
2022-01-23 19:43:54 -0700

    starting project

---
+++ app.js
@@ -0,0 +1,12 @@
+function start() {
+    document.getElementById("start").innerHTML = "Lets get ready to rumble";
+}
+function stop(){
+    document.getElementById("stop").innerHTML = "Stop rumbling";
+}
+function reset(){
+    document.getElementById("reset").innerHTML = "Reset";
+}
+function clear(){
+    document.getElementById("start").innerHTML = '';
+}
